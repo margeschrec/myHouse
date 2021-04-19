b7b7cde73ca6d582abb1d0d7ba4760ea366ed7aa
margeschrec <margeschrec@gmail.com>
2021-04-19 15:06:24 -0400

    completed first draft of index.php

--- index.php
+++ index.php
@@ -9,7 +9,21 @@ Welcome
 </head>
 <body>
 <div class="container-fluid" id="indexBg">
-
+<div class="indexImgs" id="indexChair">
+    <img src="../img/index_chair.png" class="img-fluid">
+</div>
+<div class="indexImgs" id="indexLamp">
+    <img src="../img/index_lamp.png" class="img-fluid">
+</div>
+<div class="indexImgs" id="indexTable">
+    <img src="../img/index_table.png" class="img-fluid">
+</div>
+<div class="indexImgs" id="indexClock">
+    <img src="../img/clock.png" class="img-fluid">
+</div>
+<div class="indexImgs" id="indexCalen">
+    <img src="../img/index_calender.jpg" class="img-fluid">
+</div>
 </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
 </body>
