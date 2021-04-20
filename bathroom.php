4bb68b1f431c6625dc1d0dbf00b250d131f67f52
margeschrec <margeschrec@gmail.com>
2021-04-19 22:00:39 -0400

    finished first draft of bathroom.php

---
+++ bathroom.php
@@ -0,0 +1,30 @@
+<html>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>
+        Welcome
+    </title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/main.css" />
+</head>
+
+<body>
+    <div class="container-fluid" id="bathroomBg">
+        <div class="bathroomImgs" id="indexMirror">
+            <img src="../img/bathroom_mirror.png" class="img-fluid">
+        </div>
+        <div class="bathroomImgs" id="indexToilet">
+            <img src="../img/bathroom_toilet.png" class="img-fluid">
+        </div>
+        <div class="bathroomImgs" id="indexTp">
+            <img src="../img/bathroom_tp.png" class="img-fluid">
+        </div>
+        <div class="bathroomImgs" id="indexRug">
+            <img src="../img/bathroom_bathmat.png" class="img-fluid">
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+</body>
+
+</html>
