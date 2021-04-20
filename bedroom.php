b67adf0ace022fa9255bef7fed85600c29e67e8e
margeschrec <margeschrec@gmail.com>
2021-04-20 13:50:49 -0400

    completed kitchen bedroom php, started jquery file

--- bedroom.php
+++ bedroom.php
@@ -3,33 +3,28 @@
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>
-        Welcome
+        Goodnight
     </title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
     <link rel="stylesheet" href="css/main.css" />
 </head>
 
 <body>
-    <div class="container-fluid" id="indexBg">
-        <div class="indexImgs" id="indexChair">
-            <img src="../img/index_chair.png" class="img-fluid">
+    <div class="container-fluid" id="bedroomBg">
+        <div class="indexImgs" id="bedroomBag">
+            <img src="../img/bedroom_bag.png" class="img-fluid">
         </div>
-        <div class="indexImgs" id="indexLamp">
-            <img src="../img/index_lamp.png" class="img-fluid">
+        <div class="indexImgs" id="bedroomDiary">
+            <img src="../img/bedroom_diary.png" class="img-fluid">
         </div>
-        <div class="indexImgs" id="indexTable">
-            <img src="../img/index_table.png" class="img-fluid">
+        <div class="indexImgs" id="bedroomRug">
+            <img src="../img/bedroom_rug.png" class="img-fluid">
         </div>
-        <div class="indexImgs" id="indexClock">
+        <div class="indexImgs" id="bedroomClock">
             <img src="../img/clock.png" class="img-fluid">
-        </div>
-        <div class="indexImgs" id="indexCalen">
-            <img src="../img/index_calender.jpg" class="img-fluid">
         </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="js/main.js"></script>
 </body>
 
 </html>
