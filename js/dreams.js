cd48898ce3388a20b600eac73a4961fd3b6878ee
margeschrec <margeschrec@gmail.com>
2021-05-15 19:51:22 -0400

    started nightmare game, need to debug and implement into bedroom

---
+++ js/dreams.js
@@ -0,0 +1,50 @@
+const square = document.querySelectorAll(".square");
+const vision = document.querySelectorAll(".vision");
+const timeLeft = document.querySelector("#time-left");
+let score = document.querySelector("#score");
+
+let result = 0;
+let currentTime = timeLeft.textContent
+
+function randomSquare() {
+  square.forEach((classname) => {
+    classname.classList.remove("vision");
+  });
+  let randomPosition = square[Math.floor(Math.random() * 9)];
+  randomPosition.classList.add("vision");
+
+  //asign the id of the randomPooition to hitPosition for us to use later
+  let hitPosition = randomPosition.id;
+  return hitPosition;
+}
+
+var hitScore = randomSquare();
+
+  square.forEach((id) => {
+    id.addEventListener("mouseup", () => {
+      if (id.id === hitScore) {
+        result++;
+        score.textContent = result;
+      }
+    });
+  });
+
+
+function moveVision() {
+  let timerId = null;
+  timerId = setInterval(randomSquare, 1000);
+}
+
+moveVision();
+
+function countDown() {
+    currentTime--
+    timeLeft.textContent = currentTime
+
+    if(currentTime === 0 ) {
+        clearInterval(timerId)
+        alert('Wakey Wakey')
+    }
+}
+
+let timerId = setInterval(countDown, 1000)
